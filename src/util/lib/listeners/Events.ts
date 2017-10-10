f732eaaa28c6da9034d4d9e359563021b3aeb7af
Mike Wohlrab <Mike@Layer7.Solutions>
2017-10-10 17:52:27 -0500

    Update maint command

--- src/util/lib/listeners/Events.ts
+++ src/util/lib/listeners/Events.ts
@@ -364,11 +364,12 @@ export class Events {
 										`\n\n` +
 										`Please note the following planned Destiny maintenance event(s).` +
 										`\n\n` +
-										`__**Tuesday October 3rd, 2017 [2017-10-03]**__\n` +
+										`__**Wednesday October 11th, 2017 [2017-10-11]**__\n` +
 										`**STARTS:** 8 AM PDT (3 PM UTC) \n` +
-										`**ENDS:** 12 PM PDT (7 PM UTC)` +
+										`**ENDS:** 2 PM PDT (9 PM UTC) \n` +
+										`**NO DOWNTIME IS EXPECTED. Details:** <https://twitter.com/bungiehelp/status/917824850835628032>` +
 										`\n\n` +
-										`Note: Maintenance times, including end time, are subject to change by Bungie without notice. For more info see @BungieHelp on Twitter or https://www.bungie.net/en/Help/Article/13125. Thank you.`);
+										`Note: Maintenance times, including end time, are subject to change by Bungie without notice. For more info see @BungieHelp on Twitter or https://www.bungie.net/en/Help/Article/13125 Thank you.`);
 					return;
 			}
 		}
